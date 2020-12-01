483146dac41a01034f57eb20ed33d53a608570c4
Ivan Dimitrov <biobeehive@abv.bg>
2020-12-01 15:23:36 +0200

    Initial Commit

---
+++ Musik/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Musik
+//
+//  Created by Ivan Dimitrov on 1.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Musik/MusikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusikApp.swift
+//  Musik
+//
+//  Created by Ivan Dimitrov on 1.12.20.
+//
+
+import SwiftUI
+
+@main
+struct MusikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
